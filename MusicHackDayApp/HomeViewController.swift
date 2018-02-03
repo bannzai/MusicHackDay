18954ac27d564fa641e69bafe6245542f5970448
bannzai <kingkong999yhirose@gmail.com>
2018-02-03 22:27:38 +0900

    :metal: add start screen

---
+++ MusicHackDayApp/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  MusicHackDayApp
+//
+//  Created by Yudai.Hirose on 2018/02/03.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+public class HomeViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- MusicHackDayApp/ViewController.swift
+++ MusicHackDayApp/ViewController.swift
@@ -1,5 +1,4 @@
-//
-//  ViewController.swift
+// //  ViewController.swift
 //  MusicHackDayApp
 //
 //  Created by Yudai.Hirose on 2018/02/03.
@@ -8,36 +7,67 @@
 
 import UIKit
 
+enum Result {
+    case success([String: Any]) // json
+    case failure(String) // error message
+}
+
+struct API {
+    static func request(url: String, completion: @escaping (Result) -> Void) {
+        let url = URL(string: url)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let data = data, let response = response {
+                    print("response: \(response)")
+                    do {
+                        guard let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: Any] else {
+                            completion(.failure("Can't convert json"))
+                            return
+                        }
+                        completion(.success(json))
+                        print("json: \(json)")
+                    } catch {
+                        
+                        completion(.failure("Serialize Error"))
+                    }
+                } else {
+                    completion(.failure(error?.localizedDescription ?? "Unknown response error"))
+                }
+            }
+        }
+        task.resume()
+
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: false)
     }
-
-    @IBAction func buttonPressed(_ sender: Any) {
-//        let urlSession = URLSession(configuration: .default)
-        let url = URL(string: "https://taptappun.net/hackathon/musichackday2018/authentication/signin")!
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let data = data, let response = response {
-                print(response)
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
-                    print(json)
-                } catch {
-                    print("Serialize Error")
-                }
-            } else {
-                print(error ?? "Error")
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        API.request(url:  "https://taptappun.net/hackathon/musichackday2018/authentication/signin") { (result) in
+            switch result {
+            case .success(_):
+                let viewController = UIStoryboard(name: "HomeViewController", bundle: nil).instantiateInitialViewController()!
+                self.navigationController?.pushViewController(viewController, animated: true)
+            case .failure(let errorMessage):
+                print("error: \(errorMessage)")
             }
         }
-        
-        task.resume()
     }
 }
 
